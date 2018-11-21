8d061b41aaa9ae50cba39dbde05f1e1a274b4788
enzo1920 <enzo1920@ya.ru>
2018-11-21 17:31:28 +0300

    dz3 работа с классами

---
+++ dz3/api.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import abc
+import json
+import datetime
+import logging
+import hashlib
+import uuid
+import re
+from optparse import OptionParser
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+SALT = "Otus"
+ADMIN_LOGIN = "admin"
+ADMIN_SALT = "42"
+OK = 200
+BAD_REQUEST = 400
+FORBIDDEN = 403
+NOT_FOUND = 404
+INVALID_REQUEST = 422
+INTERNAL_ERROR = 500
+ERRORS = {
+    BAD_REQUEST: "Bad Request",
+    FORBIDDEN: "Forbidden",
+    NOT_FOUND: "Not Found",
+    INVALID_REQUEST: "Invalid Request",
+    INTERNAL_ERROR: "Internal Server Error",
+}
+UNKNOWN = 0
+MALE = 1
+FEMALE = 2
+GENDERS = {
+    UNKNOWN: "unknown",
+    MALE: "male",
+    FEMALE: "female",
+}
+
+
+# от него наследуются другие классы
+class Field(object):
+    def __init__(self, required=False, nullable=False):
+        self.required = required
+        self.nullable = nullable
+
+    def checker(self, value):
+        raise NotImplementedError
+
+
+class CharField(Field):
+    def checker(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Value not valid  type str")
+        return value
+
+
+class ArgumentsField(Field):
+    def checker(self, value):
+        if not isinstance(value, dict):
+            raise ValueError("Value not valid  type dict")
+        return value
+
+
+class EmailField(CharField):
+    def checker(self, value):
+        value = super(EmailField, self).checker(value)
+        if re.match("^.+@([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", value) != None:
+            raise ValueError("Email address not valid")
+        return value
+
+
+class PhoneField(Field):
+    def checker(self, value):
+        if not re.match(r'(^7[\d]{10}$)', str(value)):
+            raise ValueError("Phone number not valid")
+        return value
+
+
+class DateField(Field):
+    def checker(self, value):
+        try:
+            value = datetime.strptime(value, '%d.%m.%Y')
+        except Exception:
+            raise ValueError("DateFielde not valid ")
+        return value
+
+
+class BirthDayField(DateField):
+    def checker(self, value):
+        value = super(BirthDayField.self).checker(value)
+        if (datetime.today() - value).days / 365.2425 > 70:
+            raise ValueError("Birth Day error not valid ")
+        return value
+
+
+class GenderField(object):
+    pass
+
+
+class ClientIDsField(object):
+    pass
+
+
+class ClientsInterestsRequest(object):
+    client_ids = ClientIDsField(required=True)
+    date = DateField(required=False, nullable=True)
+
+
+class OnlineScoreRequest(object):
+    first_name = CharField(required=False, nullable=True)
+    last_name = CharField(required=False, nullable=True)
+    email = EmailField(required=False, nullable=True)
+    phone = PhoneField(required=False, nullable=True)
+    birthday = BirthDayField(required=False, nullable=True)
+    gender = GenderField(required=False, nullable=True)
+
+
+class MethodRequest(object):
+    account = CharField(required=False, nullable=True)
+    login = CharField(required=True, nullable=True)
+    token = CharField(required=True, nullable=True)
+    arguments = ArgumentsField(required=True, nullable=True)
+    method = CharField(required=True, nullable=False)
+
+    @property
+    def is_admin(self):
+        return self.login == ADMIN_LOGIN
+
+
+def check_auth(request):
+    if request.is_admin:
+        digest = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT).hexdigest()
+    else:
+        digest = hashlib.sha512(request.account + request.login + SALT).hexdigest()
+    if digest == request.token:
+        return True
+    return False
+
+
+def method_handler(request, ctx, store):
+    response, code = None, None
+    return response, code
+
+
+class MainHTTPHandler(BaseHTTPRequestHandler):
+    router = {
+        "method": method_handler
+    }
+    store = None
+
+    def get_request_id(self, headers):
+        return headers.get('HTTP_X_REQUEST_ID', uuid.uuid4().hex)
+
+    def do_POST(self):
+        response, code = {}, OK
+        context = {"request_id": self.get_request_id(self.headers)}
+        request = None
+        try:
+            data_string = self.rfile.read(int(self.headers['Content-Length']))
+            request = json.loads(data_string)
+        except:
+            code = BAD_REQUEST
+
+        if request:
+            path = self.path.strip("/")
+            logging.info("%s: %s %s" % (self.path, data_string, context["request_id"]))
+            if path in self.router:
+                try:
+                    response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
+                except Exception, e:
+                    logging.exception("Unexpected error: %s" % e)
+                    code = INTERNAL_ERROR
+            else:
+                code = NOT_FOUND
+
+        self.send_response(code)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        if code not in ERRORS:
+            r = {"response": response, "code": code}
+        else:
+            r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
+        context.update(r)
+        logging.info(context)
+        self.wfile.write(json.dumps(r))
+        return
+
+
+if __name__ == "__main__":
+    op = OptionParser()
+    op.add_option("-p", "--port", action="store", type=int, default=8080)
+    op.add_option("-l", "--log", action="store", default=None)
+    (opts, args) = op.parse_args()
+    logging.basicConfig(filename=opts.log, level=logging.INFO,
+                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+    server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
+    logging.info("Starting server at %s" % opts.port)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    server.server_close()
