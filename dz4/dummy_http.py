d82c59c45f1abfb6f4e18c849bcedb028d257df9
enzo1920 <enzo1920@ya.ru>
2018-12-05 14:41:42 +0300

    dummy 6 process

--- dz4/dummy_http.py
+++ dz4/dummy_http.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 
-import multiprocessing
-import threading
+import argparse
 import datetime
 import logging
+import multiprocessing
+import os
 import select
 import socket
 import time
-import os
 
-
-
+from http_request import HttpRequest
+from http_response import *
 
 EOL1 = b'\n\n'
 EOL2 = b'\n\r\n'
@@ -18,91 +18,110 @@ RESPONSE = b'HTTP/1.0 200 OK\r\nDate: Mon, 1 Jan 1996 01:01:01 GMT\r\n'
 RESPONSE += b'Content-Type: text/plain\r\nContent-Length: 13\r\n\r\n'
 RESPONSE += b'Hello, world!'
 
+EPOLLEXCLUSIVE = 1 << 28
 LOG_DIR = "./log"
 
 
 class HTTPServ(object):
-
-    def __init__(self, host, port, max_connections):
+    def __init__(self, host, port, root_dir):
         self.host = host
         self.port = port
-        self.max_connections = max_connections
-        self.connections = {}
-        self.requests = {}
-        self.responses = {}
+        self.rtdir = root_dir
 
     def create_servsock(self):
         self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         self.serversocket.bind((self.host, self.port))
-        self.serversocket.listen(1)
+        self.serversocket.listen(5000)
         self.serversocket.setblocking(0)
+        print('srv pid:{}'.format(str(os.getpid())))
+        return self.serversocket
 
-    def worker(self):
-        self.epoll = select.epoll()
-        self.epoll.register(self.serversocket.fileno(), select.EPOLLIN)
-        logging.info(os.getpid())
-        try:
-            while True:
-                events = self.epoll.poll(1)
-                for fileno, event in events:
-                    #logging.info("fn :{} pid {}".format(str(fileno),str(os.getpid())))
-                    if fileno == self.serversocket.fileno():
+    def epol_connect(self):
+        ed = select.epoll()
+        ed.register(self.serversocket, select.EPOLLIN | EPOLLEXCLUSIVE)
+        connections = {}
+        requests = {}
+        responses = {}
+        while True:
+
+            events = ed.poll()
+            for fileno, event in events:
+                if fileno == self.serversocket.fileno():
+                    try:
                         connection, address = self.serversocket.accept()
+                        print(address)
                         connection.setblocking(0)
-                        self.epoll.register(connection.fileno(), select.EPOLLIN)
-                        self.connections[connection.fileno()] = connection
-                        self.requests[connection.fileno()] = b''
-                        self.responses[connection.fileno()] = RESPONSE
-                    elif event & select.EPOLLIN:
-                        self.requests[fileno] += self.connections[fileno].recv(1024)
-                        if EOL1 in self.requests[fileno] or EOL2 in self.requests[fileno]:
-                            self.epoll.modify(fileno, select.EPOLLOUT)
-                            print('-' * 40 + '\n' + self.requests[fileno].decode()[:-2])
-                    elif event & select.EPOLLOUT:
-                        byteswritten = self.connections[fileno].send(self.responses[fileno])
-                        self.responses[fileno] = self.responses[fileno][byteswritten:]
-                        if len(self.responses[fileno]) == 0:
-                            self.epoll.modify(fileno, 0)
-                            self.connections[fileno].shutdown(socket.SHUT_RDWR)
-                    elif event & select.EPOLLHUP:
-                        self.epoll.unregister(fileno)
-                        self.connections[fileno].close()
-                        del self.connections[fileno]
-        except Exception as exc:
-            logging.error(exc)
-        finally:
-            self.epoll.unregister(self.serversocket.fileno())
-            self.epoll.close()
-            self.serversocket.close()
+                        ed.register(connection.fileno(), select.EPOLLIN)
+                        connections[connection.fileno()] = connection
+                        requests[connection.fileno()] = b''
+                        responses[connection.fileno()] = RESPONSE
+
+                    except self.serversocket.error as ex:
+                        logging.error('err_accept:{}'.format(ex))
+                elif event & select.EPOLLIN:
+                    try:
+                        requests[fileno] += connections[fileno].recv(1024)
+                        if EOL1 in requests[fileno] or EOL2 in requests[fileno]:
+                            ed.modify(fileno, select.EPOLLOUT)
+                            # TODO handle input data here
+                            print('-' * 40 + '\n' + requests[fileno].decode()[:-2])
+                    except Exception as ex:
+                        logging.error('erorror read:{}'.format(ex))
+                elif event & select.EPOLLOUT:
+                    try:
+                        byteswritten = connections[fileno].send(responses[fileno])
+                        responses[fileno] = responses[fileno][byteswritten:]
+                        if len(responses[fileno]) == 0:
+                            ed.modify(fileno, 0)
+                            connections[fileno].shutdown(socket.SHUT_RDWR)
+                    except Exception as ex:
+                        logging.error('err_write:{}'.format(ex))
+                elif event & select.EPOLLHUP:
+                    try:
+                        ed.unregister(fileno)
+                        connections[fileno].close()
+                        del connections[fileno]
+                    except Exception as ex:
+                        logging.error('error close:{}'.format(ex))
+
 
 
 def main():
-    worklog_file = os.path.join(LOG_DIR,
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', help='root dir', default='./www')
+    parser.add_argument('-l', help='log dir', default='./log')
+    parser.add_argument('-p', help='port', type=int, default=8080)
+    parser.add_argument('-w', help='worker', type=int, default=6)
+    args = parser.parse_args()
+    if not os.path.exists(args.l):
+        os.makedirs(args.l)
+    worklog_file = os.path.join(args.l,
                                 'dummy_http_log-{0}.{1}'.format(datetime.datetime.now().strftime("%Y.%m.%d_%H-%M-%S"),
                                                                 'log'))
     logging.basicConfig(filename=worklog_file, level=logging.INFO,
                         format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
-    srv = HTTPServ('188.227.18.141', 8080, 5)
+    srv = HTTPServ('188.227.18.141', args.p, args.r)
     srv.create_servsock()
     logging.info("server started")
     logging.info(os.getpid())
     try:
-        # workers = []
-        process_list = [multiprocessing.Process(target=srv.worker, args=()) for i in range(5)]
-        print(process_list)
+        process_list = [multiprocessing.Process(target=srv.epol_connect, args=()) for i in range(args.w)]
         for i, p in enumerate(process_list):
             p.start()
-            logging.info('process {} will start'.format(i + 1))
+            logging.info('process {}  started'.format(i + 1))
             logging.info(p.is_alive())
+            print(p)
             time.sleep(0.1)
-            # p.join()
+        for p in process_list:
+            p.join()
 
 
     except KeyboardInterrupt:
         print(process_list)
         for worker_p in process_list:
-            print (worker_p)
+            print(worker_p)
             if worker_p:
                 worker_p.terminate()
 
