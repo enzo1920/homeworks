2aed74de0aec100de73ade18fe349dfce82b08df
enzo1920 <enzo1920@ya.ru>
2018-12-09 22:24:45 +0300

    tested

--- dz4/dummy_http.py
+++ dz4/dummy_http.py
@@ -20,113 +20,113 @@ SERVER_NAME = 'dummy_http'
 
 
 class HTTPServ(object):
-    def __init__(self, host, port, root_dir,server_name):
-        self.host = host
-        self.port = port
-        self.rtdir = root_dir
-        self.srv_name = server_name
+  def __init__(self, host, port, root_dir,server_name):
+    self.host = host
+    self.port = port
+    self.rtdir = root_dir
+    self.srv_name = server_name
 
-    def create_servsock(self):
-        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        self.serversocket.bind((self.host, self.port))
-        self.serversocket.listen(50)
-        self.serversocket.setblocking(0)
-        print('srv pid:{}'.format(str(os.getpid())))
+  def create_servsock(self):
+    self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    self.serversocket.bind((self.host, self.port))
+    self.serversocket.listen(50)
+    self.serversocket.setblocking(0)
+    print('srv pid:{}'.format(str(os.getpid())))
 
-    def ephandle(self):
-        ed = select.epoll()
-        ed.register(self.serversocket, select.EPOLLIN | EPOLLEXCLUSIVE)
-        connections = {}
-        requests = {}
-        responses = {}
-        while True:
+  def ephandle(self):
+    ed = select.epoll()
+    ed.register(self.serversocket, select.EPOLLIN | EPOLLEXCLUSIVE)
+    connections = {}
+    requests = {}
+    responses = {}
+    while True:
 
-            events = ed.poll()
-            for fileno, event in events:
-                if fileno == self.serversocket.fileno():
-                    try:
-                        connection, address = self.serversocket.accept()
-                        connection.setblocking(0)
-                        ed.register(connection.fileno(), select.EPOLLIN)
-                        connections[connection.fileno()] = connection
-                        requests[connection.fileno()] = HttpRequest()
-                    except Exception as ex:
-                        logging.error('err_accept:{}'.format(ex))
-                elif event & select.EPOLLIN:
-                    # handle input data
-                    try:
-                        data = connections[fileno].recv(1024)
-                        req = requests[fileno]
-                        req.add_data(data)
-                        if req.is_ready:
-                            ed.modify(fileno, select.EPOLLOUT)
-                            responses[fileno] = HttpResponse(req, self.rtdir, self.srv_name)
-                    except Exception as ex:
-                        logging.exception('erorror read:{}'.format(ex))
-                elif event & select.EPOLLOUT:
-                    try:
-                        connection = connections[fileno]
-                        resp = responses[fileno]
-                        data = resp.read(1024)
-                        nbytes = connection.send(data)
-                        resp.seek(nbytes)
-                        if resp.is_empty():
-                            connection.shutdown(socket.SHUT_RDWR)
-                            ed.unregister(fileno)
-                            requests.pop(fileno, None)
-                            responses.pop(fileno, None)
-                            connection = connections.pop(fileno, None)
-                            if connection:
-                                  connection.close()
-                    except Exception as ex:
-                        logging.exception('err_write:{}'.format(ex))
-                elif event & select.EPOLLHUP:
-                    try:
-                        ed.unregister(fileno)
-                        connections[fileno].close()
-                        del connections[fileno]
-                    except Exception as ex:
-                        logging.exception('error close:{}'.format(ex))
+      events = ed.poll()
+      for fileno, event in events:
+        if fileno == self.serversocket.fileno():
+          try:
+            connection, address = self.serversocket.accept()
+            connection.setblocking(0)
+            ed.register(connection.fileno(), select.EPOLLIN)
+            connections[connection.fileno()] = connection
+            requests[connection.fileno()] = HttpRequest()
+          except Exception as ex:
+            logging.error('err_accept:{}'.format(ex))
+        elif event & select.EPOLLIN:
+          # handle input data
+          try:
+            data = connections[fileno].recv(1024)
+            req = requests[fileno]
+            req.add_data(data)
+            if req.is_ready:
+              ed.modify(fileno, select.EPOLLOUT)
+              responses[fileno] = HttpResponse(req, self.rtdir, self.srv_name)
+          except Exception as ex:
+            logging.exception('erorror read:{}'.format(ex))
+        elif event & select.EPOLLOUT:
+          try:
+            connection = connections[fileno]
+            resp = responses[fileno]
+            data = resp.read(1024)
+            nbytes = connection.send(data)
+            resp.seek(nbytes)
+            if resp.is_empty():
+              connection.shutdown(socket.SHUT_RDWR)
+              ed.unregister(fileno)
+              requests.pop(fileno, None)
+              responses.pop(fileno, None)
+              connection = connections.pop(fileno, None)
+              if connection:
+                  connection.close()
+          except Exception as ex:
+            logging.exception('err_write:{}'.format(ex))
+        elif event & select.EPOLLHUP:
+          try:
+            ed.unregister(fileno)
+            connections[fileno].close()
+            del connections[fileno]
+          except Exception as ex:
+            logging.exception('error close:{}'.format(ex))
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-r', help='root dir', default='./www')
-    parser.add_argument('-l', help='log dir', default='./log')
-    parser.add_argument('-p', help='port', type=int, default=8080)
-    parser.add_argument('-w', help='worker', type=int, default=6)
-    args = parser.parse_args()
-    if not os.path.exists(args.l):
-        os.makedirs(args.l)
-    worklog_file = os.path.join(args.l,
-                                'dummy_http_log-{0}.{1}'.format(datetime.datetime.now().strftime("%Y.%m.%d_%H-%M-%S"),
-                                                                'log'))
-    logging.basicConfig(filename=worklog_file, level=logging.INFO,
-                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
-    srv = HTTPServ('188.227.18.141', args.p, args.r, SERVER_NAME)
-    srv.create_servsock()
-    logging.info("server started")
-    logging.info(os.getpid())
-    try:
-        process_list = [multiprocessing.Process(target=srv.ephandle, args=()) for i in range(args.w)]
-        for i, p in enumerate(process_list):
-            p.start()
-            logging.info('process {}  started'.format(i + 1))
-            logging.info(p.is_alive())
-            print(p)
-            time.sleep(0.1)
-        for p in process_list:
-            p.join()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-r', help='root dir', default='./www')
+  parser.add_argument('-l', help='log dir', default='./log')
+  parser.add_argument('-p', help='port', type=int, default=8080)
+  parser.add_argument('-w', help='worker', type=int, default=6)
+  args = parser.parse_args()
+  if not os.path.exists(args.l):
+    os.makedirs(args.l)
+  worklog_file = os.path.join(args.l,
+                'dummy_http_log-{0}.{1}'.format(datetime.datetime.now().strftime("%Y.%m.%d_%H-%M-%S"),
+                                'log'))
+  logging.basicConfig(filename=worklog_file, level=logging.INFO,
+            format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+  srv = HTTPServ('188.227.18.141', args.p, args.r, SERVER_NAME)
+  srv.create_servsock()
+  logging.info("server started")
+  logging.info(os.getpid())
+  try:
+    process_list = [multiprocessing.Process(target=srv.ephandle, args=()) for i in range(args.w)]
+    for i, p in enumerate(process_list):
+      p.start()
+      logging.info('process {}  started'.format(i + 1))
+      logging.info(p.is_alive())
+      print(p)
+      time.sleep(0.1)
+    for p in process_list:
+      p.join()
 
 
-    except KeyboardInterrupt:
-        print(process_list)
-        for worker_p in process_list:
-            print(worker_p)
-            if worker_p:
-                worker_p.terminate()
+  except KeyboardInterrupt:
+    print(process_list)
+    for worker_p in process_list:
+      print(worker_p)
+      if worker_p:
+        worker_p.terminate()
 
 if __name__ == '__main__':
-    main()
+  main()
--- dz4/http_response.py
+++ dz4/http_response.py
@@ -10,126 +10,124 @@ NOT_FOUND = 404
 INVALID_REQUEST = 422
 INTERNAL_ERROR = 500
 ERRORS = {
-    BAD_REQUEST: "Bad Request",
-    FORBIDDEN: "Forbidden",
-    NOT_FOUND: "Not Found",
-    INVALID_REQUEST: "Invalid Request",
-    INTERNAL_ERROR: "Internal Server Error",
+  BAD_REQUEST: "Bad Request",
+  FORBIDDEN: "Forbidden",
+  NOT_FOUND: "Not Found",
+  INVALID_REQUEST: "Invalid Request",
+  INTERNAL_ERROR: "Internal Server Error",
 }
 
 CONTENT_TYPE = {
-    'html': 'text/html',
-    'css': 'text/css',
-    'js': 'application/javascript',
-    'jpg': 'image/jpeg',
-    'jpeg': 'image/jpeg',
-    'png': 'image/png',
-    'gif': 'image/gif',
+  'html': 'text/html',
+  'css': 'text/css',
+  'js': 'application/javascript',
+  'swf': 'application/x-shockwave-flash',
+  'jpg': 'image/jpeg',
+  'jpeg': 'image/jpeg',
+  'png': 'image/png',
+  'gif': 'image/gif',
 }
 
 
 class Resource(object):
+  def __init__(self, path):
+    self.path = path
+    self.type = None
+    self.length = None
+    self.data = None
 
-    def __init__(self, path):
-        self.path = path
-        self.type = None
-        self.length = None
-        self.data = None
-
-    def load(self):
-        self.type = CONTENT_TYPE.get(self.path.rsplit('.', 1)[-1])
-        with open(self.path, 'rb') as f:
-            self.data = f.read()
-        self.length = len(self.data)
+  def load(self):
+    self.type = CONTENT_TYPE.get(self.path.rsplit('.', 1)[-1])
+    with open(self.path, 'rb') as f:
+      self.data = f.read()
+    self.length = len(self.data)
 
 
 class HttpResponse(object):
 
-    def __init__(self, req, root_directory,srv_name):
-        self.data = None
-        self.cur = 0
-        self.headers = {}
-        self.resource = None
-        self.root_dir = os.path.abspath(root_directory)
-        self.srv_name = srv_name
-        self.build_response(req)
+  def __init__(self, req, root_directory,srv_name):
+    self.data = None
+    self.cur = 0
+    self.headers = {}
+    self.resource = None
+    self.root_dir = os.path.abspath(root_directory)
+    self.srv_name = srv_name
+    self.build_response(req)
 
-    def build_response(self, req):
+  def build_response(self, req):
+    self.set_headers()
+    if not req.is_valid:
+      self.render4xx(400)
+    elif req.method in ('HEAD', 'GET'):
+      path = self.path_from_url(req.url)
+      if path[:len(self.root_dir)] != self.root_dir:
+        self.render4xx(403)
+        return
+      self.load_resource(path)
+      if self.resource:
+        self.data = 'HTTP/1.0 200 OK\r\n'
         self.set_headers()
-        if not req.is_valid:
-            self.render4xx(400)
-        elif req.method in ('HEAD', 'GET'):
-            path = self.path_from_uri(req.uri)
-            # print(req.method)
-            if path[:len(self.root_dir)] != self.root_dir:
-                print(path[:len(self.root_dir)], self.root_dir)
-                self.render4xx(403)
-                return
-            self.load_resource(path)
-            if self.resource:
-                self.data = 'HTTP/1.0 200 OK\r\n'
-                self.set_headers()
-                self.render_headers()
-                if req.method == 'GET':
-                    self.render_body()
-            else:
-                if req.uri[-1] == '/':
-                    self.render4xx(403)
-                else:
-                    self.render4xx(404)
-        else:
-            self.render4xx(405)
-
-    def render4xx(self, code):
-        self.data = data = 'HTTP/1.1 {} {} '.format(code, ERRORS.get(code))
         self.render_headers()
+        if req.method == 'GET':
+          self.render_body()
+      else:
+        if req.url[-1] == '/':
+          self.render4xx(403)
+        else:
+          self.render4xx(404)
+    else:
+      self.render4xx(405)
 
-    def path_from_uri(self, uri):
-        if uri[-1] == '/':
-            uri += 'index.html'
-        if uri[0] == '/':
-            uri = uri[1:]
-        path = urllib.unquote(uri).decode('utf-8')
-        path = path.split('?', 1)[0]
-        path = os.path.join(self.root_dir, path)
-        # print(os.path.abspath(path))
-        return os.path.abspath(path)
+  def render4xx(self, code):
+    self.data = 'HTTP/1.1 {} {} '.format(code, ERRORS.get(code))
+    self.render_headers()
 
-    def load_resource(self, path):
-        if os.path.exists(path) and os.path.isfile(path):
-            self.resource = Resource(path)
-            self.resource.load()
+  def path_from_url(self, url):
+    if url[-1] == '/':
+      url += 'index.html'
+    if url[0] == '/':
+      url = url[1:]
+    path = urllib.unquote(url).decode('utf-8')
+    path = path.split('?', 1)[0]
+    path = os.path.join(self.root_dir, path)
+    print(os.path.abspath(path))
+    return os.path.abspath(path)
 
-    def set_headers(self):
-        self.headers['Connection'] = 'close'
-        self.headers['Date'] = self.httpdate(DateTime.utcnow())
-        self.headers['Server'] = self.srv_name
-        if self.resource:
-            if self.resource.type:
-                self.headers['Content-Type'] = self.resource.type
-            self.headers['Content-Length'] = self.resource.length
+  def load_resource(self, path):
+    if os.path.exists(path) and os.path.isfile(path):
+      self.resource = Resource(path)
+      self.resource.load()
 
-    def render_headers(self):
-        for name, value in self.headers.items():
-            self.data += '%s: %s\r\n' % (name, value)
-        self.data += '\r\n'
+  def set_headers(self):
+    self.headers['Connection'] = 'close'
+    self.headers['Date'] = self.httpdate(DateTime.utcnow())
+    self.headers['Server'] = self.srv_name
+    if self.resource:
+      if self.resource.type:
+        self.headers['Content-Type'] = self.resource.type
+      self.headers['Content-Length'] = self.resource.length
 
-    def render_body(self):
-        self.data += self.resource.data
+  def render_headers(self):
+    for name, value in self.headers.items():
+      self.data += '%s: %s\r\n' % (name, value)
+    self.data += '\r\n'
 
-    def read(self, nbytes):
-        return self.data[self.cur: self.cur + nbytes]
+  def render_body(self):
+    self.data += self.resource.data
 
-    def seek(self, nbytes):
-        self.cur += nbytes
+  def read(self, nbytes):
+    return self.data[self.cur: self.cur + nbytes]
 
-    def is_empty(self):
-        return self.cur >= len(self.data)
+  def seek(self, nbytes):
+    self.cur += nbytes
 
-    @staticmethod
-    def httpdate(dt):
-        weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][dt.weekday()]
-        month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
-                 "Oct", "Nov", "Dec"][dt.month - 1]
-        return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (weekday, dt.day, month,
-                                                        dt.year, dt.hour, dt.minute, dt.second)
+  def is_empty(self):
+    return self.cur >= len(self.data)
+
+  @staticmethod
+  def httpdate(dt):
+    weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][dt.weekday()]
+    month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+         "Oct", "Nov", "Dec"][dt.month - 1]
+    return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (weekday, dt.day, month,
+                            dt.year, dt.hour, dt.minute, dt.second)
