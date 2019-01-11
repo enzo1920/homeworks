7d01506a655e2b5288add2cde33997834ecfa02b
enzo1920 <enzo1920@ya.ru>
2019-01-11 14:05:34 +0300

    dz7 print in gradient check

--- dz7/dmia/gradient_check.py
+++ dz7/dmia/gradient_check.py
@@ -49,5 +49,5 @@ def grad_check_sparse(f, x, analytic_grad, num_checks):
         grad_analytic = analytic_grad[ix]
         rel_error = abs(grad_numerical - grad_analytic) / (
         abs(grad_numerical) + abs(grad_analytic))
-        print ('numerical: %f analytic: %f, relative error: %e' % (
+        print('numerical: %f analytic: %f, relative error: %e' % (
         grad_numerical, grad_analytic, rel_error))
